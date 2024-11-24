8f7992610207da509e3e5f4f613d56d5992c07aa
fatapia2552 <fatapia@uninorte.edu.co>
2024-11-24 10:58:12 -0500

    Terminado TransactionController

--- src/core/controllers/TransactionController.java
+++ src/core/controllers/TransactionController.java
@@ -54,12 +54,100 @@ public class TransactionController {
     
     
     public static Response Withdraw(String sourceacc, String amount) {
-        
+        try {
+            double amountInt;
+
+          
+            AccountsStorage accstorage = AccountsStorage.getInstance();
+            Account sourceAccount = accstorage.getAccount(sourceacc);
+            if (sourceacc.equals("")) {
+                return new Response("Source Account must not be empty", Status.BAD_REQUEST);
+            }
+            if (sourceAccount == null) {
+                return new Response("Source Account does not exist", Status.NOT_FOUND);
+            }
+
+            
+            try {
+                amountInt = Double.parseDouble(amount);
+                if (amountInt <= 0) {
+                    return new Response("Amount must be positive", Status.BAD_REQUEST);
+                }
+            } catch (NumberFormatException ex) {
+                return new Response("Amount must be numeric", Status.BAD_REQUEST);
+            }
+
+            // Verificar saldo suficiente
+            if (sourceAccount.getBalance() < amountInt) {
+                return new Response("Insufficient funds", Status.BAD_REQUEST);
+            }
+
+           
+            sourceAccount.withdraw(amountInt);
+
+           
+            TransactionsStorage transtorage = TransactionsStorage.getInstance();
+            if (!transtorage.addTransaction(new Transaction(TransactionType.WITHDRAW, sourceAccount, null, amountInt))) {
+                return new Response("Transaction could not be recorded", Status.NOT_IMPLEMENTED);
+            }
+
+            return new Response("Transaction completed successfully", Status.CREATED);
+
+        } catch (Exception ex) {
+            return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
+        }
     }
     
     
     
     public static Response Transfer(String sourceacc, String destacc, String amount) {
-        
+        try {
+            double amountInt;
+
+            
+            AccountsStorage accstorage = AccountsStorage.getInstance();
+            Account sourceAccount = accstorage.getAccount(sourceacc);
+            Account destinationAccount = accstorage.getAccount(destacc);
+
+            if (sourceacc.equals("") || destacc.equals("")) {
+                return new Response("Source and Destination Accounts must not be empty", Status.BAD_REQUEST);
+            }
+            if (sourceAccount == null) {
+                return new Response("Source Account does not exist", Status.NOT_FOUND);
+            }
+            if (destinationAccount == null) {
+                return new Response("Destination Account does not exist", Status.NOT_FOUND);
+            }
+
+            
+            try {
+                amountInt = Double.parseDouble(amount);
+                if (amountInt <= 0) {
+                    return new Response("Amount must be positive", Status.BAD_REQUEST);
+                }
+            } catch (NumberFormatException ex) {
+                return new Response("Amount must be numeric", Status.BAD_REQUEST);
+            }
+
+            
+            if (sourceAccount.getBalance() < amountInt) {
+                return new Response("Insufficient funds in Source Account", Status.BAD_REQUEST);
+            }
+
+           
+            sourceAccount.withdraw(amountInt);
+            destinationAccount.deposit(amountInt);
+
+            
+            TransactionsStorage transtorage = TransactionsStorage.getInstance();
+            if (!transtorage.addTransaction(new Transaction(TransactionType.TRANSFER, sourceAccount, destinationAccount, amountInt))) {
+                return new Response("Transaction could not be recorded", Status.NOT_IMPLEMENTED);
+            }
+
+            return new Response("Transaction completed successfully", Status.CREATED);
+
+        } catch (Exception ex) {
+            return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
+        }
     }
 }
